0b606b1e4d55dea27804f29ad9beb0c42b32fa64
Dominik Mikolajczyk <jokerekv2@gmail.com>
2020-10-09 17:11:08 +0200

    Solution

--- src/Calculations.java
+++ src/Calculations.java
@@ -1,3 +1,26 @@
+import static java.lang.Math.*;
+
 public class Calculations {
+    public boolean isEven(int n) {
+        if ((n % 2) == 0) {
+            return true;
+        } else
+            return false;
+    }
+
+    public boolean isOdd(int n) {
+        if ((n % 2) != 0) {
+            return true;
+        } else
+            return false;
+    }
+
+    public double circleField(double r) {
+        return PI * pow(r, 2);
+    }
+
+    public double power(int n) {
+        return pow(n,2);
+    }
 
 }
